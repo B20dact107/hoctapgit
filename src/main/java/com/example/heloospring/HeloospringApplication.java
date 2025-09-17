5b505b7dd7bd533fc24053a4dc7b0dcfdfeaf55a
Admin <daovanlam2002@gmail.com>
2025-09-18 00:35:57 +0700

    fix duplication create source doamin

---
+++ src/main/java/com/example/heloospring/HeloospringApplication.java
@@ -0,0 +1,13 @@
+package com.example.heloospring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeloospringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeloospringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/heloospring/HeloospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.heloospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeloospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
